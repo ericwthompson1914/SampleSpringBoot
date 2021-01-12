cd327957d5323d9fb701fd764410c18c1b0cb2aa
ericwthompson1914 <77356912+ericwthompson1914@users.noreply.github.com>
2021-01-12 14:34:38 -0500

    Add files via upload
    Initial commit

---
+++ src/main/java/com/example/SpringController.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class SpringController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Enter a number 1-10 in the url bar to see one of my author recommendations.\nEx: http://localhost:8080/{number}";
+	}
+
+	@GetMapping("/{id}")
+	public String getRecommendation(@PathVariable String id) {
+		switch (id) {
+		case "1":
+			return "Brandon Sanderson";
+		case "2":
+			return "Brent Weeks";
+		case "3":
+			return "Brian McClellan";
+		case "4":
+			return "J.R.R. Tolkein";
+		case "5":
+			return "John Flanagan";
+		case "6":
+			return "Lee Child";
+		case "7":
+			return "Robert Jordan";
+		case "8":
+			return "Robin Hobb";
+		case "9":
+			return "Terry Brooks";
+		case "10":
+			return "Vince Flynn";
+		}
+		return "Entera number 1-10";
+	}
+}
